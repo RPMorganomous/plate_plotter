3e0de2ce66c5bfc1786b1696e3d64a9680d13ed5
KedroBoss <dimmak90@gmail.com>
2017-06-13 17:08:56 +0300

    Team Model: Initial

--- plot_app/models.py
+++ plot_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# TODO: If any custom user information - make a custom user model
+
+# Users can create teams
+class Team(models.Model):
+    name = models.CharField(max_length=64)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
