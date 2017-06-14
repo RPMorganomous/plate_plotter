4d50735f8d53b33ac73a45c52d344aec8992c416
KedroBoss <dimmak90@gmail.com>
2017-06-14 13:00:05 +0300

    Authentication: Setup

--- plate_plotter/urls.py
+++ plate_plotter/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
+from plot_app.views import profile
 
 urlpatterns = [
-    url(r'^', include('plot_app.urls')),
-    url(r'^admin/', admin.site.urls),
-]
+                  url(r'^', include('plot_app.urls')),
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^accounts/', include('registration.backends.simple.urls')),
+                  url(r'^accounts/profile/$', profile, name='profile'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- plot_app/views.py
+++ plot_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'plot/index.html')
+
+
+def profile(request):
+    return render(request, 'plot/profile.html')
